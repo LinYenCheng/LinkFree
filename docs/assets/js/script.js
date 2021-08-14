b968bd9193786c3123943c5acd4497d691ebe30c
LinYenCheng <breaking0215@hotmail.com>
2021-08-14 08:52:49 +0800

    Update

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -55,24 +55,81 @@ if (/Line/.test(navigator.userAgent)) {
 
 // Google Tag Manager
 
-(function (w, d, s, l, i) {
-  w[l] = w[l] || [];
-  w[l].push({
-    "gtm.start": new Date().getTime(),
-    event: "gtm.js",
-  });
-  var f = d.getElementsByTagName(s)[0],
-    j = d.createElement(s),
-    dl = l != "dataLayer" ? "&l=" + l : "";
-  j.async = true;
-  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-  f.parentNode.insertBefore(j, f);
-  
-})(window, document, "script", "dataLayer", "GTM-W5LMXCR");
+setTimeout(() => {
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      "gtm.start": new Date().getTime(),
+      event: "gtm.js",
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-W5LMXCR");
 
-document.onreadystatechange = function () {
-  if (document.readyState == "complete") {
-    document.getElementById('mode').click();
+  var runningOnBrowser = typeof window !== "undefined";
+  var isBot =
+    (runningOnBrowser && !("onscroll" in window)) ||
+    (typeof navigator !== "undefined" &&
+      /(gle|ing|ro|msn)bot|crawl|spider|yand|duckgo/i.test(
+        navigator.userAgent
+      ));
+  var supportsIntersectionObserver =
+    runningOnBrowser && "IntersectionObserver" in window;
+  function loadLikeCo() {
+    //新增的 loadLikeCo() 函數
+    (function () {
+      var likeCo = document.createElement("script");
+      likeCo.type = "text/javascript";
+      likeCo.defer = true;
+      likeCo.src = "//static.like.co/sdk/v1/button.js";
+      (
+        document.getElementsByTagName("head")[0] ||
+        document.getElementsByTagName("body")[0]
+      ).appendChild(likeCo);
+    })();
   }
-}
 
+  if (!isBot && supportsIntersectionObserver) {
+    var link_observer = new IntersectionObserver(
+      function (entries) {
+        if (entries[0].isIntersecting) {
+          loadLikeCo();
+          link_observer.disconnect();
+        }
+      },
+      { threshold: [0] }
+    );
+    link_observer.observe(document.getElementById("fb-root"));
+  } else {
+    loadLikeCo();
+  }
+
+  // lazyloadCSS
+  function lazyloadCSS() {
+    const a = document.createElement("link");
+    a.rel = "stylesheet";
+    a.href =
+      "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+
+    // const b = document.createElement('link');
+    // b.rel = 'stylesheet';
+    // b.href = 'fonts/basic_latin/mplus_webfonts.css';
+
+    const z = document.getElementsByTagName("head")[0];
+    z.parentNode.insertBefore(a, z);
+    // z.parentNode.insertBefore(b, z);
+  }
+
+  const raf =
+    requestAnimationFrame ||
+    mozRequestAnimationFrame ||
+    webkitRequestAnimationFrame ||
+    msRequestAnimationFrame;
+  if (raf) raf(lazyloadCSS);
+  else window.addEventListener("load", lazyloadCSS);
+  // lazyloadCSS
+}, 2000);
